4bf4827e6812236ec424a016da2ab48299763d92
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2022-01-27 11:30:00 -0600

    added from master

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import FavoritesView from '../FavoritesView/FavoritesView';
 
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
-      <FavoritesView />
     </div>
   );
 }
