0bb90e9de0c00d7bbeb2d02cce6bda21797aa54e
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2022-01-27 11:30:30 -0600

    nav

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import FavoritesView from "../FavoritesView/FavoritesView";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 function App(props) {
   return (
-    <div>
-      <h1>Giphy Search!</h1>
-    </div>
+    <Router>
+      <div>
+        <ul className="nav">
+          <li>
+            <Link to="/">Search</Link>
+          </li>
+          <li>
+            <Link to="/favorites">Checkout</Link>
+          </li>
+        </ul>
+        <Route exact path="/">
+          
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesView />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
