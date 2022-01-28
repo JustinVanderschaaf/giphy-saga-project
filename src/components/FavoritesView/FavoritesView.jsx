d49f26bf6ac92a3e06d4f6eef9d493d22df8cfa6
Christopher Razidlo <cd.razidlo@gmail.com>
2022-01-28 11:18:40 -0600

    added mui styling and grid to favorites view

--- src/components/FavoritesView/FavoritesView.jsx
+++ src/components/FavoritesView/FavoritesView.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import FavoritesItem from './FavoritesItem/FavoritesItem';
 
+// Import MUI components
+import Grid from '@mui/material/Grid';
+
 function FavoritesView() {
 
     // Store access, dispatch hook
@@ -10,13 +13,13 @@ function FavoritesView() {
     const dispatch = useDispatch();
 
     return (
-        <div>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
             {favorites.map(gif => (
-                <>
-                <FavoritesItem key={gif.id} gif={gif} />
-                </>
+                <Grid item xs={2} sm={4} md={4}>
+                    <FavoritesItem key={gif.id} gif={gif} />
+                </Grid>
             ))}
-        </div>
+        </Grid>
     )
 }
 
